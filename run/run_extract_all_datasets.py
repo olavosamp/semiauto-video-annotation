b493dfa2ec2de76e7445f94a7990923b3441885e
Olavo <olavosamp@poli.ufrj.br>
2019-06-23 22:03:45 -0300

    Minor modification

--- run/run_extract_all_datasets.py
+++ run/run_extract_all_datasets.py
@@ -6,6 +6,8 @@ import libs.dirs    as dirs
 import libs.commons as commons
 from libs.index     import IndexManager
 
+f = lambda x: Path(x)
+
 mainIndexPath = Path(dirs.index) / "main_index.csv"
 newIndexPath  = Path(dirs.index) / "unlabeled_index.csv"
 
@@ -14,6 +16,7 @@ ind2  = IndexManager(path=newIndexPath)
 
 # Get video paths in Main Index (labeled videos)
 labeledVideos = list(ind1.index.VideoPath.unique())
+labeledVideos = list(map(f, labeledVideos))
 for video in labeledVideos:
     print(video)
 print("Labeled videos: ", len(labeledVideos))
@@ -26,9 +29,7 @@ for format in commons.videoFormats:
     globList = glob(datasetPath + "/**" + "/*."+format, recursive=True)
     allVideos.extend(globList)
 
-for i in range(len(allVideos)):
-    allVideos[i] = Path(allVideos[i]).relative_to(datasetPath)
-
+allVideos = list(map(f, allVideos))
 for video in allVideos:
     print(video)
 print("Total videos: ", len(allVideos))
@@ -42,3 +43,8 @@ for video in unlabeledVideos:
     print(video)
 print("Unlabeled videos: ", len(unlabeledVideos))
 print("\n")
+
+print("\n")
+print(allVideos[-2])
+print(labeledVideos[-1])
+print(allVideos[-2] == labeledVideos[-1])
