0f128c3a6984ede403c562b483a459e0613a0681
Olavo <olavosamp@poli.ufrj.br>
2020-02-14 12:40:05 -0300

    Add data saving to xls on plot_confusion_matrix

--- models/plot_confusion_matrix.py
+++ models/plot_confusion_matrix.py
@@ -1,14 +1,15 @@
-## Plot all confusion matrixes
-import numpy                as np
-from glob                   import glob
-from pathlib                import Path
+# Plot all confusion matrixes
+import pandas as pd
+import numpy as np
+from glob import glob
+from pathlib import Path
 
-import libs.dirs            as dirs
-import libs.utils           as utils
-import libs.dataset_utils   as dutils
-import models.utils         as mutils
-import libs.commons         as commons
-from libs.vis_functions     import plot_confusion_matrix
+import libs.dirs as dirs
+import libs.utils as utils
+import libs.dataset_utils as dutils
+import models.utils as mutils
+import libs.commons as commons
+from libs.vis_functions import plot_confusion_matrix
 
 ''' Prints all confusion matrixes. Uses history files found under folder
 results/history_<dataset>_rede_<net_number>_val_<val_type>/'''
@@ -19,7 +20,10 @@ if __name__ == "__main__":
     # net_type = dutils.get_input_network_type(commons.network_types)
     # val_type = dutils.get_input_network_type(commons.val_types, message='validation set')
     # rede = int(input("\nEnter net number.\n"))
+    confMatFolder = Path(dirs.results) / "confusion_matrix"
+    dfPath = confMatFolder / "stats_table.xlsx"
 
+    seriesList = []
     for net_type in commons.network_types.values():
         for val_type in commons.val_types.values():
             for rede in commons.net_target_column.keys():
@@ -27,17 +31,17 @@ if __name__ == "__main__":
                 datasetPath = Path(dirs.dataset) / \
                     "{}_dataset_rede_{}_val_{}".format(net_type, rede, val_type)
                 datasetName = datasetPath.stem
-                confMatPath = dirs.results + "/confusion_matrix/" + \
-                    "confusion_matrix_" + str(datasetName) + ".pdf"
+                confMatPath = confMatFolder / \
+                    ("confusion_matrix_" + str(datasetName) + ".pdf")
 
                 modelFolder = Path(dirs.saved_models) / \
-                        "{}_{}_epochs".format(datasetName, numEpochs)
+                    "{}_{}_epochs".format(datasetName, numEpochs)
                 historyFolder = Path(dirs.saved_models) / \
-                        "history_{}_{}_epochs".format(datasetName, numEpochs)
+                    "history_{}_{}_epochs".format(datasetName, numEpochs)
 
                 # Load history files
                 globString = str(historyFolder / "history_run*pickle")
-                histFiles  = glob(globString)
+                histFiles = glob(globString)
 
                 bestValLoss = np.inf
                 bestConfMat = None
@@ -46,16 +50,16 @@ if __name__ == "__main__":
 
                     # Get best epoch results
                     bestValIndex = np.argmin(history['loss-val'])
-                    valLoss  = history['loss-val'][bestValIndex]
+                    valLoss = history['loss-val'][bestValIndex]
                     if valLoss < bestValLoss:
                         bestValLoss = valLoss
-                        bestValAcc  = history['acc-val'][bestValIndex]
+                        bestValAcc = history['acc-val'][bestValIndex]
                         bestConfMat = history['conf-val'][bestValIndex]
-                
+                        bestValF1 = history['f1-val'][bestValIndex]
+
                 if bestConfMat is None:
                     print("No history file found in folder{}.".format(historyFolder))
                     continue
-                print(bestConfMat)
                 if val_type == 'ref':
                     valName = 'Reference'
                 elif val_type == 'semiauto':
@@ -63,7 +67,31 @@ if __name__ == "__main__":
                 netName = net_type[0].upper() + net_type[1:]
                 title = "Level {} | Dataset {} | Val {}".format(rede, netName, valName)
 
+                acc = mutils.compute_class_acc(bestConfMat)
+
+                print("\n", title)
+                print(bestConfMat)
+                print("Mean Acc: ", np.mean(acc))
+                print("Std  Acc: ", np.std(acc))
+                print("Acc: ", acc)
+                print("F1:  ", bestValF1)
                 labels = commons.net_labels[rede]
 
+                if rede == 3:
+                    entrySeries = pd.Series({'rede': rede,
+                                             'dataset': net_type,
+                                             'validation': val_type,
+                                             'mean_acc': np.mean(acc),
+                                             'std_acc':  np.std(acc),
+                                             'f1_anode':  bestValF1[0],
+                                             'f1_damage': bestValF1[1],
+                                             'f1_buried': bestValF1[2],
+                                             'f1_flange': bestValF1[3],
+                                             'f1_repair': bestValF1[4]
+                                             })
+                    seriesList.append(entrySeries)
+
                 plot_confusion_matrix(bestConfMat, title=title, labels=labels, normalize=normalize, show=False,
-                                        save_path=confMatPath)
+                                      save_path=confMatPath)
+    statsDf = pd.DataFrame(seriesList)
+    statsDf.to_excel(dfPath)
